018a22cae998359bbea3a525a63c2dd0d933ee71
SeungYeop Yang <seungyeop.yang@gmail.com>
2025-11-01 07:19:03 -0500

    pytest

--- rstrip/rstrip.py
+++ rstrip/rstrip.py
@@ -27,27 +27,22 @@ def trim_trailing_spaces(filename=None):
     try:
         if filename:
             with open(filename, "r") as file:
-                string_rstrip = line_strip(file)
+                line_strip(file)
         else:
-            string_rstrip = line_strip(sys.stdin)
+            line_strip(sys.stdin)
 
     except Exception as e:
         print(f"Error: {e}")
         exit(1)
 
-    return string_rstrip
-
 
 def line_strip(file):
     """
-    return the string after rstrip
+    rstrip the string
 
-    Python interpreter will raise a MemoryError if the size of string_strip grows beyond the limit.
     """
-    string_rstrip = str()
     for line in file:
-        string_rstrip += line.rstrip()
-    return string_rstrip
+        print(line.rstrip(), end="")
 
 
 def main():
@@ -57,11 +52,9 @@ def main():
             print_help()
             return
         else:
-            string_rstrip = trim_trailing_spaces(arg)
+            trim_trailing_spaces(arg)
     else:
-        string_rstrip = trim_trailing_spaces()
-
-    print(string_rstrip, end="")
+        trim_trailing_spaces()
 
 
 if __name__ == "__main__":
